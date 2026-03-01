940b22578ff42d6a241956f5026ba0d64a7acf7a
nisha-shetty03 <nisha.shetty093@gmail.com>
2026-03-01 12:36:22 +0530

    updating backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://cattle-breed-detector-wbup.onrender.com//predict", {
         method: "POST",
         body: formData,
       });
